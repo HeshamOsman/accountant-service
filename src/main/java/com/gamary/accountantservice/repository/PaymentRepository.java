665c0239ed86454e95a2f46b0e30534ba02b4a29
Hesham Osman <heshamosman@Heshams-MacBook-Pro.local>
2020-12-17 11:35:53 +0100

    Remove

--- src/main/java/com/gamary/accountantservice/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gamary.accountantservice.repository;
-
-import com.gamary.accountantservice.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment,String> {
-}
--- src/main/java/com/gamary/accountantservice/repository/ProcessedPaymentRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gamary.accountantservice.repository;
-
-import com.gamary.accountantservice.entity.ProcessedPaymentRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProcessedPaymentRequestRepository extends JpaRepository<ProcessedPaymentRequest,String> {
-}
