0e71b875dc3cf3baff25f78e970678b09961516f
Hesham Osman <heshamosman@Heshams-MacBook-Pro.local>
2020-12-17 10:09:04 +0100

    Extract modelmapper config to seperate class

--- src/main/java/com/gamary/accountantservice/AccountantServiceApplication.java
+++ src/main/java/com/gamary/accountantservice/AccountantServiceApplication.java
@@ -24,54 +24,5 @@ public class AccountantServiceApplication {
 	}
 
 
-	@Bean
-	public ModelMapper modelMapper(){
 
-		ModelMapper modelMapper = new ModelMapper();
-
-		Provider<ZonedDateTime> zonedDateTimeProvider = new AbstractProvider<ZonedDateTime>() {
-			@Override
-			public ZonedDateTime get() {
-				return ZonedDateTime.now();
-			}
-		};
-
-		Converter<String, ZonedDateTime> toZonedDateTime = new AbstractConverter<String, ZonedDateTime>() {
-			@Override
-			protected ZonedDateTime convert(String source) {
-				ZonedDateTime zonedDateTime = ZonedDateTime.parse(source);
-				return zonedDateTime;
-			}
-		};
-
-
-		modelMapper.createTypeMap(String.class, ZonedDateTime.class);
-		modelMapper.addConverter(toZonedDateTime);
-		modelMapper.getTypeMap(String.class, ZonedDateTime.class).setProvider(zonedDateTimeProvider);
-
-		Provider<LocalDate> localDateProvider = new AbstractProvider<LocalDate>() {
-			@Override
-			public LocalDate get() {
-				return LocalDate.now();
-			}
-		};
-
-		Converter<String, LocalDate> toStringDate = new AbstractConverter<String, LocalDate>() {
-			@Override
-			protected LocalDate convert(String source) {
-				DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-				LocalDate localDate = LocalDate.parse(source, format);
-				return localDate;
-			}
-		};
-
-
-		modelMapper.createTypeMap(String.class, LocalDate.class);
-		modelMapper.addConverter(toStringDate);
-		modelMapper.getTypeMap(String.class, LocalDate.class).setProvider(localDateProvider);
-		modelMapper.createTypeMap(PaymentRequestDTO.class, ProcessedPaymentRequestDTO.class).addMapping(PaymentRequestDTO::getId,ProcessedPaymentRequestDTO::setReferenceId);
-
-
-		return modelMapper;
-	}
 }
